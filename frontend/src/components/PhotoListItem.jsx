3f5c3d98f12c581a08266a918c7986b857b3e146
RussellAbraham <russell_a@live.com>
2023-09-14 18:55:19 -0700

    Build a PhotoListItem component using the sample data provided

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -15,7 +15,33 @@ const sampleDataForPhotoListItem = {
 };
 
 const PhotoListItem = () => {
+  const {
+    id,
+    location: { city, country },
+    imageSource,
+    username,
+    profile,
+  } = sampleDataForPhotoListItem;
+  
   /* Insert React */
+
+  return (
+    <div className="photo-list-item">
+      <img src={imageSource} alt={`Photo by ${username}`} />
+      <div className="photo-details">
+        <div className="photographer-info">
+          <img id={id} src={profile} alt={`${username}'s profile`} />
+          <p className="username">{username}</p>
+        </div>
+        <div className="location-info">
+          <p className="location">
+            {city}, {country}
+          </p>
+          <p className="photo-id">ID: {id}</p>
+        </div>
+      </div>
+    </div>    
+  );
 };
 
 export default PhotoListItem;
