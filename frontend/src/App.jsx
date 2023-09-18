3854cb7400a6402b258a91db2f6ba63110e16b3d
RussellAbraham <russell_a@live.com>
2023-09-17 19:27:59 -0700

    Refactored components to pass props down from mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import HomeRoute from 'routes/HomeRoute';
 
+import topics from 'mocks/topics';
+import photos from 'mocks/photos';
+
+
 import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute topics={topics} photos={photos} />
     </div>
   );
 };
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,27 +3,9 @@ import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
-const TopicList = () => {
-  
-  const topics = sampleDataForTopicList.map((topic, index)=>{
+const TopicList = (props) => {
+  const {topics} = props;
+  const topicsToRender = topics.map((topic, index)=>{
     return (
       <TopicListItem 
         key={index}
@@ -34,7 +16,7 @@ const TopicList = () => {
 
   return (
     <div className="top-nav-bar__topic-list">
-      {topics}
+      {topicsToRender}
     </div>
   );
 };
